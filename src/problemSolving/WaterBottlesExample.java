9fcb57bf936b13628a22e0cf3f91b2d1e0c16e7b
Sumit Chavan <sumitchavan1137@gmail.com>
2026-08-15 23:13:56 +0530

    Water Bottle Problem 1518

---
+++ src/problemSolving/WaterBottlesExample.java
@@ -0,0 +1,25 @@
+package problemSolving;
+
+class WaterBottoles{
+	public int WaterBottoles(int numBottles,int numExchange) {
+		int ans = numBottles;
+		while(numBottles >= numExchange) {
+			int newBottles = numBottles/numExchange;
+			int remBottles = numBottles%numExchange;
+			ans = ans+ newBottles;
+			numBottles = newBottles +remBottles;
+			
+			
+		}
+		return ans;
+	}
+}
+public class WaterBottlesExample {
+	public static void main(String[] args) {
+		
+		WaterBottoles w  = new WaterBottoles();
+		int res = w.WaterBottoles(9, 3);
+		System.out.println(res);
+		
+	}
+}
